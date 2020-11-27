1c4218a8841e5d9f226886d6a9f9a2235053d6d8
Alejandro <alejobecerra1995@gmail.com>
2020-11-26 20:21:26 -0500

    added object notes

--- Objetos/desestructuración_obj.js
+++ Objetos/desestructuración_obj.js
@@ -11,15 +11,28 @@ var alejandro = {
  // se pone var {nombre} = alejandro
  // de esta forma la función sabe que va a llamar el nombre de la variable alejandro. 
 
-function imprimirNombreEnMayus (persona){
-    var {nombre} = alejandro;
-    console.log(nombre).toUpperCase();
-}
-
-
-imprimirNombreEnMayus(alejandro);
+///function imprimirNombreEnMayus (persona){
+///    var {nombre} = alejandro;
+///    console.log(nombre).toUpperCase();
+///}
+///
+///
+///imprimirNombreEnMayus(alejandro);
 
 
 // reto imprimir nombre y edad, imprimiendo "Hola me llamo Alejandro y tengo 28 años";
 // usando un objeto del que se saque la información que poble la función
 
+
+var usuario = {
+    nombre: "Alejandro",
+    edad : 25
+};
+
+function imprimirNombreYEdad (usuario){
+   var {nombre} = usuario;
+   var {edad} = usuario; 
+    console.log (`Hola me llamo ${nombre} y tengo ${edad} años`);
+}
+
+imprimirNombreYEdad(usuario);
