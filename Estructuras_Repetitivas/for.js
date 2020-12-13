310c7c3de5b817afeb05d69ee5b1c71335d5c9bd
Alejandro <alejobecerra1995@gmail.com>
2020-12-13 10:25:32 -0500

    added constant for days of the year and replaced in the for loop

--- Estructuras_Repetitivas/for.js
+++ Estructuras_Repetitivas/for.js
@@ -17,14 +17,14 @@ console.log(`Al inicio del año ${alejandro.nombre} pesaba ${alejandro.peso} kg.
 ///
 
 const VARIACION_PESO = Math.random(0,2.5);
-
- const  AUMENTAR_DE_PESO = (persona)=> persona.peso = persona.peso + VARIACION_PESO;
- const  BAJAR_DE_PESO = (persona)=> persona.peso = persona.peso - VARIACION_PESO;
+const DIAS_DEL_ANIO = 365;
+const AUMENTAR_DE_PESO = (persona)=> persona.peso = persona.peso + VARIACION_PESO;
+const BAJAR_DE_PESO = (persona)=> persona.peso = persona.peso - VARIACION_PESO;
 // La estructura que llevan los párentesis del for es la siguiente:
 // primero se crea la variable contadora "i", se separa con ; y se pone la condición
 // en este caso que "i" sea menor o igual a 365, y por último se separa con ; y se pone que el contador sume 1 al valor
 // de "i" cada vez que itere. Esto se puede hacer de dos formas: i++ o i = i + 1.
-    for(var i = 1; i <= 365; i = i + 1) {
+    for(var i = 1; i <= DIAS_DEL_ANIO; i = i + 1) {
         var random = Math.random(0,1);
         if (random < 0.25){
             AUMENTAR_DE_PESO(alejandro);
