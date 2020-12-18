ebd2f629d3febfc458d66888d3e2947d374f77b0
Alejandro <alejobecerra1995@gmail.com>
2020-12-17 21:34:13 -0500

    added switch class notes

---
+++ Estructuras_Repetitivas/switch.js
@@ -0,0 +1,25 @@
+// switch permite ejecutar diferentes flujos dependiendo de multiples condiciones
+
+
+// prompt le pide al usuario que ingrese un valor que se guardará en una variable. 
+
+var signo = prompt('¿Cuál es tu signo?');
+
+// el sintax de switch es el siguiente: 
+
+switch(signo){
+    case 'acuario':
+        //
+        break
+    case 'piscis':
+        //
+        break
+    case 'cancer':
+        //
+        break
+    // default es la forma de absorber los casos que no están contemplados en el programa y ejecutar alguna sección de código para ese caso.     
+    default: 
+    //
+    break
+}
+
