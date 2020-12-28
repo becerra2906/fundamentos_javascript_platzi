f25a70a1dd6327a7d8f1aa9fa54db60ed138c74d
Alejandro <alejobecerra1995@gmail.com>
2020-12-27 23:40:05 -0500

    started jquery requests to apis for learning callbacks and async. Will continue tomorrow

---
+++ Async/callbacks_2.js
@@ -0,0 +1,7 @@
+// vamos a hacer requests con JQuery a Star Wars api para esperar la información que el servidor manda y usarla dentro del programa. 
+
+// el api es este https://swapi.dev/api/
+
+
+// content delivery network = CDN
+
