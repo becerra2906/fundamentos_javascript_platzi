6dc709ef533284145c43bbcbc37bb0e4b2d55f86
Alejandro <alejobecerra1995@gmail.com>
2020-12-29 20:13:25 -0500

    added call back classes, but decided to learn it using the course for ASYNC rather than this one to better focus on it

--- Async/callbacks_2.js
+++ Async/callbacks_2.js
@@ -5,3 +5,23 @@
 
 // content delivery network = CDN
 
+
+const API_URL ='https://swapi.dev/api/';
+
+const PEOPLE_URL = 'people/:id';
+
+const LUKE_URL = `${API_URL}${PEOPLE_URL.replace(':id',1)}`;
+
+const ops = {crossDomain: true};
+
+
+function getLuke(){ $.get(LUKE_URL,ops, function(){
+
+    console.log(arguments);
+})}
+
+
+// tengo literalmente el código del profesor pero ahora no me corre. Originalmente me estaba funcionando pero luego dejó. No entiendo por qué
+
+// creo que tengo un problema con el linking a JQUERY lo soluciono después;
+// creo que si veré el curso de JQuery aunque creo que mi enfoque va a ser JS en forma, otras formas de hacer otras consultas a apis y GQL.
