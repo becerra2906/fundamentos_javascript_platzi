7422375216f2d3d968ff3e1449c85fcf8a50f9d8
Alejandro <alejobecerra1995@gmail.com>
2020-12-29 22:35:17 -0500

    added async await notes

---
+++ Async/await.js
@@ -0,0 +1,4 @@
+// para usar async await en una función hay que marcar la función como tal con la palabra clave async antes de la misma
+
+// guardar los resultados de la petición asincrona en una variable y ponerle await Promises(all) para que solo se guarde cuando 
+// se resuelvan todas las promesas de la función asincrona.
