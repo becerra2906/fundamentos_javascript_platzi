5c4b8bdefb90afddda4a00ccdff7ccb05efb4a65
Alejandro <alejobecerra1995@gmail.com>
2020-12-29 20:29:15 -0500

    added multiple callback notes

--- Async/callbacks_2.js
+++ Async/callbacks_2.js
@@ -27,3 +27,8 @@ function getLuke(){ $.get(LUKE_URL,ops, function(){
 // creo que si veré el curso de JQuery aunque creo que mi enfoque va a ser JS en forma, otras formas de hacer otras consultas a apis y GQL.
 
 // para hacer multiples requests debo crear una función que reciba como párametro el id del elemento que quiero pedirle al api. 
+
+// Para manejar el orden las respuestas que manda un servidor cuando se le hacen requests se debe agregar una función callback a las funciones que hacen el request al backend/servidor
+// para que esta se ejecute antes de mostrar las siguientes peticiones y sus respuestas. Ahora bien, si se hace esto se pierde el paralelismo de ejecucion.
+// para lograr esto, se pone un function(){funcion_request(segundoId)} para que esta sea el call back del request y se ejecute cuando llegue el request del primero.
+// esto se logra nesting las funciones dentro de los call backs (importante que toca llamar al callback dentro de la función desde la que se genera el request al backend). (aunque esto es el llamado "call back hell") 
